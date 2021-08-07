882b25436086ca596c0b19bbf1b476943063f7f3
hirorich <ch.dim7ir@gmail.com>
2021-08-07 16:29:00 +0900

    WebSocketによるサンプル配置

--- test.py
+++ test.py
@@ -6,7 +6,8 @@
 #from test_pkg import test_browser as tester
 #from test_pkg import test_flask as tester
 #from test_pkg import test_thread as tester
-from test_pkg import test_js as tester
+#from test_pkg import test_js as tester
+from test_pkg import test_bottle as tester
 
 # main
 if __name__ == "__main__":
---
+++ test_pkg/test_bottle.py
@@ -0,0 +1,33 @@
+"""
+Flaskサンプル
+http://localhost:3500/
+
+"""
+
+import bottle
+from bottle.ext.websocket import GeventWebSocketServer
+from bottle.ext.websocket import websocket
+
+app = bottle.Bottle()
+
+@app.route('/')
+def index():
+    return bottle.static_file('index.html', root='./test_pkg/bottle_static')
+
+@app.route('/websocket', apply=[websocket])
+def bottle_websocket(ws):
+    while True:
+        msg = ws.receive()
+        if msg is not None:
+            ws.send('echo:' + msg)
+        else:
+            break
+
+app.run(
+    host='localhost',
+    port=8080,
+    reloader=True,
+    debug=True,
+    server=GeventWebSocketServer
+)
+
