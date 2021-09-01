b424544caeb2a848bf6b6d86efe8bc28092891c6
hirorich <ch.dim7ir@gmail.com>
2021-09-01 23:57:26 +0900

    残りのテスト作成

--- test/test_module.py
+++ test/test_module.py
@@ -1,4 +1,5 @@
 import json, unittest
+from datetime import datetime
 from unittest.mock import patch, MagicMock
 
 from moray import _module
@@ -279,3 +280,102 @@ class ModuleTest(unittest.TestCase):
         result, is_success = _module._call_py_func(None, None, None)
         self.assertEqual(result, 'calling python function is faild.')
         self.assertEqual(is_success, False)
+    
+    @patch('moray._module._uniqueId', MagicMock(return_value = 'uniqueId'))
+    @patch('moray._module.time.sleep', MagicMock())
+    def test_create_js_func_1(self):
+        ws = MagicMock()
+        func_name = 'func_name'
+        
+        call_msg = {}
+        call_msg[_module._ID] = 'uniqueId'
+        call_msg[_module._RETURN] = False
+        call_msg[_module._FUNC_NAME] = func_name
+        call_msg[_module._ARGS] = ('abc', 123)
+        call_msg = json.dumps(call_msg)
+        
+        _module._call_result['uniqueId'] = {
+            _module._IS_SUCCESS: True,
+            _module._RESULT: 'result'
+        }
+        
+        try:
+            call_js = _module._create_js_func(ws, func_name)
+            get_result = call_js('abc', 123)
+            result = get_result()
+        except Exception as e:
+            self.fail()
+        
+        args, kwargs = ws.send.call_args
+        self.assertEqual(args[0], call_msg)
+        self.assertEqual(result, 'result')
+    
+    @patch('moray._module._uniqueId', MagicMock(return_value = 'uniqueId'))
+    @patch('moray._module.time.sleep', MagicMock())
+    def test_create_js_func_2(self):
+        ws = MagicMock()
+        func_name = 'func_name'
+        
+        error_msg = 'result'
+        
+        _module._call_result['uniqueId'] = {
+            _module._IS_SUCCESS: False,
+            _module._RESULT: 'result'
+        }
+        
+        try:
+            call_js = _module._create_js_func(ws, func_name)
+            get_result = call_js('abc', 123)
+            result = get_result()
+        except Exception as e:
+            self.assertIs(type(e), RuntimeError)
+            self.assertEqual(e.args[0], error_msg)
+            return
+        
+        self.fail()
+    
+    @patch('moray._module._uniqueId', MagicMock(return_value = 'uniqueId'))
+    @patch('moray._module.time.sleep', MagicMock())
+    def test_create_js_func_3(self):
+        ws = MagicMock()
+        func_name = 'func_name'
+        
+        error_msg = 'time out'
+        
+        if 'uniqueId' in _module._call_result:
+            del _module._call_result['uniqueId']
+        
+        try:
+            call_js = _module._create_js_func(ws, func_name)
+            get_result = call_js('abc', 123)
+            result = get_result()
+        except Exception as e:
+            self.assertIs(type(e), RuntimeError)
+            self.assertEqual(e.args[0], error_msg)
+            return
+        
+        self.fail()
+    
+    def test_uniqueId_1(self):
+        test_datetime = datetime(2021, 2, 3, 4, 5, 6, 789)
+        test_random = 0.123
+        strong = 1000
+        result = hex(int(test_datetime.timestamp() * 1000))[2:] + hex(int(test_random * strong))[2:]
+        with (
+            patch('moray._module.datetime', MagicMock()) as dt,
+            patch('moray._module.datetime.now', MagicMock(return_value = test_datetime)) as dtn,
+            patch('moray._module.random.random', MagicMock(return_value = test_random)) as rr
+        ):
+            self.assertEqual(_module._uniqueId(), result)
+    
+    def test_uniqueId_2(self):
+        test_datetime = datetime(2021, 2, 3, 4, 5, 6, 789)
+        test_random = 0.123
+        strong = 500
+        result = hex(int(test_datetime.timestamp() * 1000))[2:] + hex(int(test_random * strong))[2:]
+        with (
+            patch('moray._module.datetime', MagicMock()) as dt,
+            patch('moray._module.datetime.now', MagicMock(return_value = test_datetime)) as dtn,
+            patch('moray._module.random.random', MagicMock(return_value = test_random)) as rr
+        ):
+            self.assertEqual(_module._uniqueId(strong), result)
